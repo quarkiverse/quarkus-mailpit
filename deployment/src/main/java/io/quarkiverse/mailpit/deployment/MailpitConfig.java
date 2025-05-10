185c27d9a7ee60181f1b9ad9c957b931572ffbde
melloware <mellowaredev@gmail.com>
2025-05-10 10:09:58 -0400

    Mailpit 1.24.2

--- deployment/src/main/java/io/quarkiverse/mailpit/deployment/MailpitConfig.java
+++ deployment/src/main/java/io/quarkiverse/mailpit/deployment/MailpitConfig.java
@@ -20,7 +20,7 @@ public interface MailpitConfig {
     /**
      * Default docker image name.
      */
-    String DEFAULT_IMAGE = "axllent/mailpit:v1.23";
+    String DEFAULT_IMAGE = "axllent/mailpit:v1.24.2";
 
     /**
      * If Dev Services for Mailpit has been explicitly enabled or disabled. Dev Services are generally enabled
